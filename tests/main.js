f0fa360c2d2cae72ea5974a490ef48350772fbb6
Alex <alexadeschamps@gmail.com>
2015-04-27 14:00:03 -0400

    fixed some mistakes/bugs

--- tests/main.js
+++ tests/main.js
@@ -1,17 +1,13 @@
 
-ejax.get({
-	url: 'http://randomthoughts.link/thoughts'
-	})
+ejax.get('http://randomthoughts.link/thoughts')
 	.send(function(thought){
 		console.log(thought);
+		console.log(thought.results[0])
 	})
 
 
-ejax.post({
-	url: 'http://randomthoughts.link/thoughts',
-	body: {info: 'success!'}
-	})
-	.send(function(msg){
+ejax.post('http://randomthoughts.link/thoughts')
+	.send({info: 'Ejax, baby!'}, function(msg){
 		console.log(msg);
 	})
 
